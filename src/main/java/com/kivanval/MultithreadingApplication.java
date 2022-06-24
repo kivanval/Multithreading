98fd6ce75e37fbc02512de8ee81fbcf0252e9920
kivanval <kivanval@gmail.com>
2022-06-24 04:09:52 +0300

    init commit

--- src/main/java/com/kivanval/MultithreadingApplication.java
+++ src/main/java/com/kivanval/MultithreadingApplication.java
@@ -11,10 +11,12 @@ public class MultithreadingApplication {
     public static void main(String[] args) throws IOException {
         Path pathDirOne = FileSystems.getDefault().getPath("src/main/resources/threadOne");
         Path pathDirTwo = FileSystems.getDefault().getPath("src/main/resources/threadTwo");
-        System.out.println(AnalyzerTextPool.builder()
-                .addThread(pathDirOne)
-                .addThread(pathDirTwo)
-                .build()
-                .execute());
+        System.out.println(
+                AnalyzerTextPool.builder()
+                        .addThread(pathDirOne)
+                        .addThread(pathDirTwo)
+                        .build()
+                        .execute()
+        );
     }
 }
