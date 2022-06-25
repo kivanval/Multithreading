f831d8710e5a95ec5a2e48edff8d5e4df296a4a6
kivanval <kivanval@gmail.com>
2022-06-25 19:39:02 +0300

    init commit

--- src/main/java/com/kivanval/thread/AnalyzerTextPool.java
+++ src/main/java/com/kivanval/thread/AnalyzerTextPool.java
@@ -4,6 +4,7 @@ import java.nio.file.Path;
 import java.util.*;
 import java.util.concurrent.*;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 import static java.util.stream.Collectors.*;
 
